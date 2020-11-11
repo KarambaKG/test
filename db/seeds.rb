a0bc57457d36e1b020b5b4776fb8c56d4045b5a7
Maksim Kuskov <karamba92911@gmail.com>
2020-11-11 18:10:12 +0600

    Added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.create(id: 1, name: 'Cars')
+Category.create(id: 2, name: 'Weapons')
+Category.create(id: 3, name: 'Without Category')
+p 'Categories created'
+
+Blog.create(id: 1, title: 'Porshe', text: 'Porshe is good', category_id: 1, join_time: Time.current)
+Blog.create(id: 2, title: 'AK', text: 'AK not bad', category_id: 2, join_time: Time.current)
+p 'Blogs created'
